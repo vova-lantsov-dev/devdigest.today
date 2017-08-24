feb71f51cd21f8f2ec7b4b1270d40a08d4869949
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2017-08-24 23:41:00 +0300

    Implement rss generating

--- src/Core/Settings.cs
+++ src/Core/Settings.cs
@@ -16,16 +16,18 @@ namespace Core
 
         public string DefaultKeywords { get; private set; }
 
-        public string FacebookImage
-        {
-            get { return $"{WebSiteUrl}images/fb_logo.png"; }
-        }
+        public string FacebookImage => $"{WebSiteUrl}images/fb_logo.png";
+
+        public string RssFeedUrl => $"{WebSiteUrl}rss";
+
+        public Guid PublicationKey { get; set; }
+        
+        public string SupportEmail { get; set; }
 
         #region Current
 
         public static Settings Current { get; private set; }
-        public Guid PublicationKey { get; set; }
-
+        
         public static void Initialize(IConfiguration configuration)
         {
             Current = new Settings
@@ -35,7 +37,8 @@ namespace Core
                 WebSiteTitle = configuration["WebSiteTitle"],
                 DefaultDescription = WebUtility.HtmlDecode(configuration["DefaultDescription"]),
                 DefaultKeywords = WebUtility.HtmlDecode(configuration["DefaultKeywords"]),
-                PublicationKey = Guid.Parse(configuration["PublicationKey"])
+                PublicationKey = Guid.Parse(configuration["PublicationKey"]),
+                SupportEmail = "dncuug@agi.net.us"
             };
         }
 
