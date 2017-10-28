74721749f5af7636144d91ad4feaabccbe6de272
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2017-10-29 00:40:55 +0300

    Add Application Insights to project

--- src/WebSite/Program.cs
+++ src/WebSite/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace WebSite
 {
@@ -19,6 +13,7 @@ namespace WebSite
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     var env = hostingContext.HostingEnvironment;
