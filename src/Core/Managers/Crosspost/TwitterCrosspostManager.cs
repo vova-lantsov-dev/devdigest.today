b6f46b6f84e4ca5f99ee54cd6686335a04b96363
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2019-05-05 10:32:28 +0300

    Fix ConvertToHashTag

--- src/Core/Managers/Crosspost/TwitterCrosspostManager.cs
+++ src/Core/Managers/Crosspost/TwitterCrosspostManager.cs
@@ -66,7 +66,7 @@ namespace Core.Managers.Crosspost
         }
 
         private static string ConvertToHashTag(string text) =>
-            text
+            "#" + text
                 .Replace(".", "")
                 .Replace("#", "")
                 .Replace(" ", "_")
