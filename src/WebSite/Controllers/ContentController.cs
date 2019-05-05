7f76a073ea64f9beb11480e9259938536f0dc3e4
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2019-05-05 22:42:38 +0300

    // build 2019 /

--- src/WebSite/Controllers/ContentController.cs
+++ src/WebSite/Controllers/ContentController.cs
@@ -26,98 +26,54 @@ namespace WebSite.Controllers
         }
 
         [Route("content/partners")]
-        public async Task<IActionResult> Partners()
-        {
-            ViewData["Title"] = Pages.Partners;
-
-            return View("~/Views/Content/Partners.cshtml");
-        }
+        public async Task<IActionResult> Partners() => View("~/Views/Content/Partners.cshtml");
 
         [Route("content/about")]
-        public async Task<IActionResult> About()
-        {
-            ViewData["Title"] = Pages.AboutProject;
-
-            return View("~/Views/Content/About.cshtml");
-        }
+        public async Task<IActionResult> About() => View("~/Views/Content/About.cshtml");
 
         [Route("content/developex-tech-club")]
-        public async Task<IActionResult> DevelopexTechClub()
-        {
-            ViewData["Title"] = Pages.DevelopexTechClub;
-
-            return View("~/Views/Content/DevelopexTechClub.cshtml");
-        }
+        public async Task<IActionResult> DevelopexTechClub() => View("~/Views/Content/DevelopexTechClub.cshtml");
 
         [Route("search")]
-        public async Task<IActionResult> Search()
-        {
-            ViewData["Title"] = Pages.Search;
-
-            return View("~/Views/Content/Search.cshtml");
-        }
+        public async Task<IActionResult> Search() => View("~/Views/Content/Search.cshtml");
 
         [Route("content/privacy")]
-        public async Task<IActionResult> Privacy()
-        {
-            return View("~/Views/Content/Privacy.cshtml");
-        }
-        
+        public async Task<IActionResult> Privacy() => View("~/Views/Content/Privacy.cshtml");
+
         [Route("content/how-to-post-vacancy")]
-        public async Task<IActionResult> HowToPostVacancy()
-        {
-            return View("~/Views/Content/HowToPostVacancy.cshtml");
-        }
-        
+        public async Task<IActionResult> HowToPostVacancy() => View("~/Views/Content/HowToPostVacancy.cshtml");
+
         [Route("content/microsoft-tech-summit-warsaw")]
-        public async Task<IActionResult> MicrosoftTechSummitWarsaw()
-        {
-            ViewData["Title"] = Pages.MicrosoftTechSummitWarsaw;
-            
-            return View("~/Views/Content/MicrosoftTechSummitWarsaw.cshtml");
-        }
-        
+        public async Task<IActionResult> MicrosoftTechSummitWarsaw() => View("~/Views/Content/MicrosoftTechSummitWarsaw.cshtml");
+
         [Route("content/cloud-developers-days")]
-        public async Task<IActionResult> CloudDevelopersDaysPoland()
-        {
-            ViewData["Title"] = Pages.CloudDevelopersDays;
-            
-            return View("~/Views/Content/CloudDevelopersDaysPoland.cshtml");
-        } 
-        
+        public async Task<IActionResult> CloudDevelopersDaysPoland() => View("~/Views/Content/CloudDevelopersDaysPoland.cshtml");
+
         [Route("content/build-2018")]
-        public async Task<IActionResult> Build2018()
-        {
-            ViewData["Title"] = Pages.Build2018;
-            
-            return View("~/Views/Content/Build2018.cshtml");
-        }
-        
+        public async Task<IActionResult> Build2018() => View("~/Views/Content/Build2018.cshtml");
+
+        [Route("content/build-2019")]
+        public async Task<IActionResult> Build2019() => View("~/Views/Content/Build2019.cshtml");
+
         [Route("user-group")]
         [Route("content/net-core-user-group")]        
-        public async Task<IActionResult> UkrainianNETCoreUserGroup()
-        {
-            ViewData["Title"] = Pages.UkrainianNETCoreUserGroup;
-            
-            return View("~/Views/Content/UkrainianNETCoreUserGroup.cshtml");
-        }
+        public async Task<IActionResult> UkrainianNETCoreUserGroup() => View("~/Views/Content/UkrainianNETCoreUserGroup.cshtml");
 
         [Route("content/xamarin-user-group")]
-        public async Task<IActionResult> XamarinUkraineUserGroup()
-        {
-            ViewData["Title"] = Pages.XamarinUkraineUserGroup;
+        public async Task<IActionResult> XamarinUkraineUserGroup() => View("~/Views/Content/XamarinUkraineUserGroup.cshtml");
 
-            return View("~/Views/Content/XamarinUkraineUserGroup.cshtml");
-        }
-        
         [Route("content/microsoft-azure-user-group")]
-        public async Task<IActionResult> MicrosoftAzureUkraineUserGroup()
-        {
-            ViewData["Title"] = Pages.MicrosoftAzureUkraineUserGroup;
+        public async Task<IActionResult> MicrosoftAzureUkraineUserGroup() => View("~/Views/Content/MicrosoftAzureUkraineUserGroup.cshtml");
+        
+        [Route("content/telegram")]
+        public async Task<IActionResult> Telegram()=> RedirectPermanent("/content/platform");
 
-            return View("~/Views/Content/MicrosoftAzureUkraineUserGroup.cshtml");
-        }
+        [Route("partners")]
+        public async Task<IActionResult> PartnersRedirect() => RedirectPermanent("/content/partners");
 
+        [Route("about")]
+        public async Task<IActionResult> AboutRedirect() => RedirectPermanent("/content/about");
+        
         [Route("content/platform")]
         public async Task<IActionResult> Platform()
         {
@@ -143,19 +99,11 @@ namespace WebSite.Controllers
                 }).ToList();
 
             var result = new List<SocialNetworkViewModel>();
+            
             result.AddRange(pages);
             result.AddRange(channels);
 
             return View("~/Views/Content/Platform.cshtml", result);
         }
-        
-        [Route("content/telegram")]
-        public async Task<IActionResult> Telegram()=> RedirectPermanent("/content/platform");
-
-        [Route("partners")]
-        public async Task<IActionResult> PartnersRedirect() => RedirectPermanent("/content/partners");
-
-        [Route("about")]
-        public async Task<IActionResult> AboutRedirect() => RedirectPermanent("/content/about");
     }
 }
