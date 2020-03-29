3ba47560ded25308d32b38e29532b64dd98db8ac
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2020-03-29 13:07:11 +0300

    Added SocialAccount class

---
+++ src/Core/Models/SocialAccount.cs
@@ -0,0 +1,10 @@
+namespace Core.Models
+{
+    public class SocialAccount
+    {
+        public string Description { get; set; }
+        public string Logo { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
