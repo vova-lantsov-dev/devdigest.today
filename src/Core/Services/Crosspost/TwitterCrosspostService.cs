30d567dfc284e539d26464957c4d3f07ea59d12d
Andrew <andrew.gubskiy@outlook.com>
2020-11-14 12:45:37 +0200

    Update TwitterCrosspostService

--- src/Core/Services/Crosspost/TwitterCrosspostService.cs
+++ src/Core/Services/Crosspost/TwitterCrosspostService.cs
@@ -2,27 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Tweetinvi;
-using Tweetinvi.Core.Models;
 using Tweetinvi.Parameters;
 
 namespace Core.Services.Crosspost
 {
     public class TwitterCrosspostService : ICrossPostService
     {
-        private static readonly Semaphore Semaphore = new Semaphore(1, 1);
+        private static readonly Semaphore Semaphore = new(1, 1);
 
         private readonly ILogger _logger;
-        private readonly string _consumerKey;
-        private readonly string _consumerSecret;
-        private readonly string _accessToken;
-        private readonly string _accessTokenSecret;
         private readonly string _name;
         private readonly IReadOnlyCollection<string> _defaultTags;
-        private TwitterClient _client;
+        private readonly TwitterClient _client;
 
         private const int MaxTweetLength = 186;
 
@@ -30,41 +26,42 @@ namespace Core.Services.Crosspost
             string consumerKey, 
             string consumerSecret,
             string accessToken,
-            string userAccessSecret,
+            string accessSecret,
             string name,
             IReadOnlyCollection<string> defaultTags,
             ILogger<TwitterCrosspostService> logger)
         {
             _logger = logger;
             _defaultTags = defaultTags;
-            _consumerKey = consumerKey;
-            _consumerSecret = consumerSecret;
-            _accessToken = accessToken;
-            _accessTokenSecret = userAccessSecret;
             _name = name;
             
-            _client = new TwitterClient(consumerKey, consumerSecret, accessToken, userAccessSecret);
+            _client = new TwitterClient(consumerKey, consumerSecret, accessToken, accessSecret);
         }
 
         public async Task Send(string message, Uri link, [NotNull] IReadOnlyCollection<string> tags)
         {
             var tagLine = string.Join(" ", _defaultTags.ToList().Union(tags));
 
-            var maxMessageLength = MaxTweetLength - tagLine.Length;
+            var maxMessageLength = MaxTweetLength - tagLine.Length - 4;
 
-            var text= $"{Substring(message, maxMessageLength)} {tagLine} {link}";
-            
+            var sb = new StringBuilder();
+            sb.AppendLine(Substring(message, maxMessageLength));
+            sb.AppendLine(tagLine);
+            sb.AppendLine();
+            sb.AppendLine(link.ToString());
+
+            var text = sb.ToString();
             try
             {
                 Semaphore.WaitOne();
 
-                await _client.Tweets.PublishTweetAsync(new PublishTweetParameters(message));
+                await _client.Tweets.PublishTweetAsync(new PublishTweetParameters(text));
                 
                 _logger.LogInformation($"Message was sent to Twitter channel `{_name}`: `{text}`");
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error in TwitterCrosspostService.Send");
+                _logger.LogError(ex, $"Error in {nameof(TwitterCrosspostService)}.{nameof(Send)}");
             }
             finally
             {
@@ -75,7 +72,9 @@ namespace Core.Services.Crosspost
         private static string Substring(string text, int length)
         {
             if (string.IsNullOrWhiteSpace(text))
+            {
                 return string.Empty;
+            }
 
             return text.Length <= length ? text : $"{text.Substring(0, length - 4)}... ";
         }
