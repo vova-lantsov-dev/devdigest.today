fab877cf1a39b203544001d463b4b67418cdd982
Andrew <andrew.gubskiy@outlook.com>
2021-11-22 23:08:44 +0200

    Update DI

--- src/WebSite/Program.cs
+++ src/WebSite/Program.cs
@@ -1,37 +1,101 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using Core;
+using Core.Repositories;
+using Core.Services;
+using Core.Services.Posting;
+using DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.WebEncoders;
+using WebSite.AppCode;
 
-namespace WebSite;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
-{
-    public static void Main(string[] args)
+
+builder.Logging
+    .AddConsole()
+    .AddAzureWebAppDiagnostics();
+
+builder.Configuration
+    .AddJsonFile($"appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+var settings = LoadSettings(builder.Environment, builder.Configuration);
+Settings.InitializeCurrentInstance(settings);
+
+// Add services to the container.
+builder.Services
+    .AddApplicationInsightsTelemetry()
+
+    .AddMemoryCache()
+
+    .AddDbContext<DatabaseContext>(options =>
     {
-        CreateHostBuilder(args).Build().Run();
-    }
+        options.UseMySql(settings.ConnectionString, ServerVersion.Parse("5.7.32-mysql"));
+    })
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host
-            .CreateDefaultBuilder(args)
-            .ConfigureLogging(builder =>
-            {
-                builder
-                    .AddConsole()
-                    .AddAzureWebAppDiagnostics();
-            })
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                var env = hostingContext.HostingEnvironment;
+    .AddSingleton(_ => settings)
 
-                config
-                    .AddJsonFile($"appsettings.json", false, true)
-                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
-                    .AddEnvironmentVariables();
-            })
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    .AddSingleton<PostingServiceFactory>()
+
+    .AddScoped<ILocalizationService, LocalizationService>()
+    .AddScoped<IPublicationService, PublicationService>()
+    .AddScoped<IUserService, UserService>()
+    .AddScoped<IVacancyService, VacancyService>()
+    .AddScoped<IPublicationRepository, PublicationRepository>()
+    .AddScoped<ISettingsRepository, SettingsRepository>()
+    .AddScoped<ISocialRepository, SocialRepository>()
+    .AddScoped<IPageRepository, PageRepository>()
+    .AddScoped<IUserRepository, UserRepository>()
+    .AddScoped<IVacancyRepository, VacancyRepository>()
+    .AddScoped<IWebAppPublicationService, WebAppPublicationService>()
+
+    .AddScoped<ILanguageAnalyzerService>(provider =>
+    {
+        var logger = provider.GetService<ILogger<LanguageAnalyzerService>>();
+
+        return new LanguageAnalyzerService(settings.CognitiveServicesTextAnalyticsKey, logger);
+    })
+                
+    .AddApplicationInsightsTelemetry(settings.ApplicationInsights.InstrumentationKey)
+
+    .Configure<WebEncoderOptions>(options =>
+    {
+        options.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.All);
+    })
+    .AddControllersWithViews();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    //app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+static Settings LoadSettings(IWebHostEnvironment hostEnvironment, IConfiguration configuration)
+{
+    var settings = new Settings();
+
+    configuration.Bind(settings);
+    settings.ConnectionString = configuration.GetConnectionString("DefaultConnection");
+    settings.WebRootPath = hostEnvironment.WebRootPath;
+            
+    return settings;
 }
